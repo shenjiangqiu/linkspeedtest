2b84aec6eb4a601c573d9611dedcd603d0c08303
Jiangqiu Shen <ivy22233qiu@live.com>
2022-04-11 18:56:26 -0400

    update

--- src/client/main.rs
+++ src/client/main.rs
@@ -8,6 +8,8 @@ fn main() {
     let addr = args()
         .nth(1)
         .unwrap_or_else(|| "localhost:2233".to_string());
+    let size_m = args().nth(2).unwrap_or_else(|| "1024".to_string());
+    let size_m = size_m.parse::<usize>().unwrap();
 
     let mut stream = TcpStream::connect(addr.clone()).unwrap();
     println!(
@@ -23,9 +25,17 @@ fn main() {
     // create 1M buffer
     let now = Instant::now();
     let mut buffer = vec![0; 1024 * 1024];
-    for _ in 0..1024 {
+    for i in 0..size_m {
         stream.read_exact(buffer.as_mut_slice()).unwrap();
+        println!("read {}M", i + 1);
+        println!(
+            "average speed: {}M/s",
+            (i + 1) as f64 / now.elapsed().as_secs_f64()
+        );
     }
-    println!("total time: {}", now.elapsed().as_millis());
-    println!("speed: {} MB/s", 1024.0 / now.elapsed().as_secs_f64());
+    println!("total time: {}s", now.elapsed().as_secs_f64());
+    println!(
+        "speed: {} MB/s",
+        size_m as f64 / now.elapsed().as_secs_f64()
+    );
 }
--- src/server/main.rs
+++ src/server/main.rs
@@ -4,6 +4,8 @@ use std::net::TcpListener;
 
 fn main() {
     let addr = args().nth(1).unwrap_or_else(|| ":::0".to_string());
+    let size_m = args().nth(2).unwrap_or_else(|| "1024".to_string());
+    let size_m = size_m.parse::<usize>().unwrap();
     let listener = TcpListener::bind(addr).unwrap();
     println!("Listening on {}", listener.local_addr().unwrap());
     if let Ok((mut stream, addr)) = listener.accept() {
@@ -15,8 +17,10 @@ fn main() {
 
         // create 1M buffer
         let buffer = vec![0; 1024 * 1024];
-        for _i in 0..1024 {
+        for i in 0..size_m {
             stream.write_all(buffer.as_slice()).unwrap();
+            println!("wrote {}M", i + 1);
+
         }
     }
 }
